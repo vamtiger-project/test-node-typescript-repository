abe37821c023b05906a525f729a47e05f572a6cd
James-Alex Matthews <vamtiger@vamtiger.com>
2018-07-14 03:14:29 +0200

    vamtiger-node-typescript-commit: Test

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "2018 July 14 - 01:33:14.442";
+declare const _default: "2018 July 14 - 03:14:18.799";
 export default _default;
--- build/index.js
+++ build/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = "2018 July 14 - 01:33:14.442";
+exports.default = "2018 July 14 - 03:14:18.799";
 //# sourceMappingURL=index.js.map
